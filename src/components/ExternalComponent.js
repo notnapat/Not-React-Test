d1d91cea3574000aa3c1971dddaea614196c7ba6
notnapat <notnapat9@gmail.com>
2022-05-18 15:26:21 +0700

    Component's-normal-class-external

---
+++ src/components/ExternalComponent.js
@@ -0,0 +1,5 @@
+function HelloComponent(){
+    return <h1>ExternalComponent</h1>
+}
+
+export default HelloComponent   
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+/*import ExternalComnent.js*/import HelloComponent from './components/ExternalComponent';
+
+// //การสร้าง Component
+// function HelloComponent() {
+//       return <h1>HelloComponent</h1>
+// }
+
+//การสร้าง Class Component
+// class HelloComponent extends React.Component{
+//       render(){
+//             return <h1>Class Component</h1>
+//       }
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <HelloComponent />
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
