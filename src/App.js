323928d745d0d2aee2a80781555d6fee3f574983
notnapat <notnapat9@gmail.com>
2022-05-26 14:30:48 +0700

    ผลรวมยอดคงเหลือ รายรับ-รายจ่าย +ReportComponent.css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@
 import Transaction from "./components/Transaction"
 import './App.css'
 import FormComponent from "./components/FormComponent"
-import { useState } from "react"  
+import { useEffect, useState } from "react"  
 import DataContext from "./components/data/DataContext"  
 import ReportComponent from "./components/ReportComponent"
+
 
 
 function App() {
@@ -15,21 +16,35 @@ function App() {
             { id: 2, title: "ค่ากิน", amount: 400 },                            
             { id: 3, title: "ค่าเดินทาง", amount: 500 }                  
       ]
-
+// ผลรวมยอดคงเหลือ รายรับ - รายจ่าย   1-1  , 2-2 ,3   +ReportComponent.css
+     //ผลรวมยอดคงเหลือ 1 ตั่งแต่ตรงนี้  ถึง--
       const [items, setItems] = useState(initdata) 
+      const [reportIncome,setReportIncome] = useState(0)
+      const [reportExpense,setReportExpense] = useState(0)
       const onAddNewItem = (newItem) => {    
             setItems((prevItem) => {                       
                   return [newItem, ...prevItem]         
             })
       }
+      useEffect(()=>{
+            const amount = items.map(items=>items.amount)
+            const income = amount.filter(element=>element>0).reduce((total,element)=>total+=element,0)
+            const expense = (amount.filter(element=>element<0).reduce((total,element)=>total+=element,0))*-1 
+            setReportIncome(income)
+            setReportExpense(expense)
+
+      } , [items,reportIncome,reportExpense])
+      //ผลรวมยอดคงเหลือ  1  ถึงตรงนี้
       return (
+            //ผลรวมยอดคงเหลือ 2 ตั้งแต่ตรงนี้ถึง--
             // DataContext and useContext  2 + object    1-2  and 1-3
             <DataContext.Provider value={        // 1 DataContext and useContext  2 + object    
                   {
-                        income : 50000,
-                        expense : 8000
+                        income : reportIncome,
+                        expense : reportExpense
                   }
             }>       {/* 1 DataContext and useContext  2 + object*/}    
+                   {/*ผลรวมยอดคงเหลือ 2 ถึงตรงนี้*/}
                   <div className="container">
                         <h1 style={design}>แอพบัญชีรายรับ-รายจ่าย</h1>
                         <ReportComponent/>
--- src/components/ReportComponent.js
+++ src/components/ReportComponent.js
@@ -1,26 +1,48 @@
 import { useContext } from "react";
 import DataContext from "./data/DataContext";
+import './ReportComeponent.css'
 
 const ReportComponent = () => {
-    const { income, expense } = useContext(DataContext)  //2 useContext 2+ object
-
-    /* 2 DataContext 2+ object  in return*/
-    /* 3 useContext 2+ object  in return*/
+  //ผลรวมยอดคงเหลือ 3
+    const { income, expense } = useContext(DataContext)
     return (
         <div>
-            <div>
-                <DataContext.Consumer>  
-                    {context => <p>รายรับ : {context.income} รายจ่าย : {context.expense}</p>}
-                </DataContext.Consumer>
+            <h4>ยอดคงเหลือ</h4>
+            <h1>{income-expense}</h1>
+            <div className="report-container">{income}
+                <div>
+                    <h4>ยอดรายรับ</h4>
+                    <p className="report plus">{income}</p>
+                </div>
+                <div>
+                    <h4>ยอดรายจ่าย</h4>
+                    <p className="report minus">{expense}</p>
+                </div>
             </div>
-            
-            <div>
-                
-                <p>รายรับ : {income}</p> 
-                <p>รายจ่าย : {expense}</p>
-            </div>
-        </div>
 
+        </div>
     )
+    // const { income, expense } = useContext(DataContext)  //2 useContext 2+ object
+
+
+
+    // /* 2 DataContext 2+ object  in return*/
+    // /* 3 useContext 2+ object  in return*/
+    // return (
+    //     <div>
+    //         <div>
+    //             <DataContext.Consumer>  
+    //                 {context => <p>รายรับ : {context.income} รายจ่าย : {context.expense}</p>}
+    //             </DataContext.Consumer>
+    //         </div>
+
+    //         <div>
+
+    //             <p>รายรับ : {income}</p> 
+    //             <p>รายจ่าย : {expense}</p>
+    //         </div>
+    //     </div>
+
+    // )
 }
 export default ReportComponent
